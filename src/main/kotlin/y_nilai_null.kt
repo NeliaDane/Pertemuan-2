aac4e69775823d8a7784a3cb2930466548be7634
[Nelia Dane] <neliadane17@gmail.com>
2022-03-20 18:40:58 +0700

    contoh Penerapan pemberian nilai null yang diizinkan:

---
+++ src/main/kotlin/y_nilai_null.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+    /*
+    Salah satu kesalahan yang banyak ditemukan pada bahasa pemrograman adalah pengaksesan
+terhadap anggota referensi null dan akan menghasilkan pesan kesalahan null reference exception,
+di Java sering disebut dengan Null Pointer Exception (NPE). Kotlin type system bertujuan untuk
+menghilangkan NPE dari kode program. Kotlin tidak mengizinkan pemberian nilai null secara tidak sah.
+Untuk dapat memberikan nilai null pada suatu variabel dapat dilakukan dengan cara menambahkan tanda ?
+diakhir tipe data.
+     */
+//Nullable types
+    var maybeNumber: Int? = 15
+    maybeNumber = null
+    println("maybeNumber: $maybeNumber ${maybeNumber?.hashCode()}")
+}
