0ba52f02f223b36a1a80dc0cb7c3085a2cedeb9d
[Nelia Dane] <neliadane17@gmail.com>
2022-03-20 18:49:37 +0700

    contoh Penerapan Ekspresi Lambdas

---
+++ src/main/kotlin/zzzzzzzzzzzzzzz_lambda.kt
@@ -0,0 +1,23 @@
+/*
+Ekspresi lambda selalu dikelilingi oleh tanda blok „{}‟ deklarasi parameter berada dalam blok
+yang memiliki opsional type annotation pada bagian tubuh blok ditandai dengan tanda „->‟. Jika
+tipe nilai kembalian dari fungsi bukan unit maka nilai kembalian diduga merupakan baris akhir
+dari program atau ekspresi dalam tubuh lambda tersebut.
+ */
+fun main() {
+    println(MyFunction("Rekayasa Perangkat Lunak"))
+//Lambda Function
+    val mylambda :(String)->Unit = {s:String->print(s)}
+    val v:String = "Jurusan Teknik Informatika"
+    mylambda(v)
+//Inline Function
+    myFun(v,mylambda) //melewati lambda sebagai parameter fungsi lain
+}
+fun MyFunction(x: String): String {
+    var c:String = "Hey!! Welcome To ---"
+    return (c+x)
+}
+fun myFun(a:String, action:(String)->Unit) { //passing lambda
+    print("\nHeyyy!!!")
+    action(a)// panggilan ke fungsi lambda
+}
